ac8f5ec86456616205209cbf845151b37f20bc0a
tam ng <minhtam.nguyen@officience.com>
2025-06-21 08:07:17 +0000

    add finance fct_order from stg-stripe-payment, readjust dim_customers to get lifetime_value

---
+++ models/marts/finance/fct_orders.sql
@@ -0,0 +1,23 @@
+With 
+orders AS 
+(
+    select *
+    from {{ ref ('stg_jaffle_shop__orders')}}
+),
+order_payments as (
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
+
+    from {{ ref("stg_stripe__payments")}}
+    group by 1
+)
+
+select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+from orders
+left join order_payments using (order_id)
